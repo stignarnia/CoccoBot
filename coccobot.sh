6bd701342102dc612ddf0bddfc0c333cd697d11a
stignarnia <diego.aldarese@gmail.com>
2021-08-30 10:44:01 +0200

    .

--- coccobot.sh
+++ coccobot.sh
@@ -3,6 +3,8 @@ while [ 1 != 0 ]
 do
 	pkill CoccoBotLinux
 	sleep 5
+	rclone copy --update --verbose --transfers 3 --checkers 3 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s "/home/ubuntu/" "google-drive:coccobot"
+	rclone copy --update --verbose --transfers 3 --checkers 3 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s "/var/www/html/" "google-drive:website"
 	wget https://github.com/stignarnia/CoccoBot/releases/latest/download/CoccoBotLinux -O /home/ubuntu/CoccoBotLinux
 	chmod +x CoccoBotLinux
 	screen ./CoccoBotLinux
